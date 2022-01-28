e56850162b500c1c0114e3d2c4bb002d7180705d
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-28 09:43:41 -0600

    finished all the collums and the items in index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag, as: 'product_info'});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {through: ProductTag, as: 'tag_info'});
 
 module.exports = {
   Product,
