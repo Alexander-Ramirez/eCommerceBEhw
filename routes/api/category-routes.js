de2d71124b93c78c891ae6e087c6859961e0cde7
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-28 11:32:49 -0600

    finished all the api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,42 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({include: [Product]}).then((categoryData) => { res.json(categoryData)}); 
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({ where: {
+      id: req.params.id,
+    }, include: [Product], }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body).then((catagoryData) => {res.json(catagoryData);})
+  .catch((err) => {res.json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body,{
+    where: {
+      id: req.params.id,
+    },
+  }).then((catagoryData) => {res.json(catagoryData);}).catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((catagoryData) => res.json(catagoryData))
 });
 
 module.exports = router;
